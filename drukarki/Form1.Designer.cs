279a278ab8922031a3604c75882c05a50bf7765c
dzeeejkooob <jakub.c.wojciechowski@gmail.com>
2025-05-07 15:52:59 +0200

    Dodaj pliki projektów.

---
+++ drukarki/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace drukarki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(321, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(211, 304);
+            this.listBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox1.Location = new System.Drawing.Point(334, 105);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 28);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Wszystkie\r\n";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem,
+            this.pomocToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(822, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // pomocToolStripMenuItem
+            // 
+            this.pomocToolStripMenuItem.Name = "pomocToolStripMenuItem";
+            this.pomocToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.pomocToolStripMenuItem.Text = "Help";
+            this.pomocToolStripMenuItem.Click += new System.EventHandler(this.pomocToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(822, 373);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Drukarki - instalator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+    }
+}
+
---
+++ drukarki/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Security.Principal;
+using System.Text;
+using System.Windows.Forms;
+
+namespace drukarki
+{
+    public partial class Form1 : Form
+    {
+        private List<(string printerName, string ipAddress)> printers = new List<(string, string)>
+        {
+            ("Pietro I Cz. B", "172.16.153.1"),
+            ("Pietro II Cz. B", "172.16.153.2"),
+            ("Pietro I Cz. A", "172.16.153.3"),
+            ("Parter Cz. A", "172.16.153.4"),
+            ("Pietro II Cz. B KOLOR", "172.16.153.5"),
+            ("Ksiegowosc", "172.16.153.6"),
+            ("Kadry", "172.16.153.8"),
+            ("Ksiegowosc B16", "172.16.153.9"),
+            ("ZZP", "172.16.153.10"),
+            ("Sekretariat KOLOR", "172.16.153.11"),
+            ("Parter Cz. B", "172.16.153.12"),
+            ("Parter Cz. B KOLOR", "172.16.153.13"),
+            ("Pietro I Cz. B KOLOR", "172.16.153.14"),
+            ("Pietro I Cz. A KOLOR", "172.16.153.15"),
+            ("Młynarska", "172.16.162.1")
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePrinterList();
+
+            if (!IsRunningAsAdministrator())
+            {
+                MessageBox.Show("Uwaga! Aplikacja nie jest uruchomiona jako administrator. Dodawanie drukarek może nie zadziałać poprawnie.", "Brak uprawnień", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+        }
+
+        private void InitializePrinterList()
+        {
+            foreach (var printer in printers)
+            {
+                listBox1.Items.Add(printer.printerName);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    listBox1.SetSelected(i, true);
+                }
+            }
+            else
+            {
+                listBox1.ClearSelected();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var originalColor = button1.BackColor;
+            button1.BackColor = Color.Orange;
+            button1.Enabled = false;
+            Cursor.Current = Cursors.WaitCursor;
+
+            StringBuilder log = new StringBuilder();
+            List<(string printerName, string ipAddress)> selectedPrinters = new List<(string, string)>();
+
+            if (checkBox1.Checked)
+            {
+                selectedPrinters.AddRange(printers);
+            }
+            else
+            {
+                if (listBox1.SelectedItems.Count == 0)
+                {
+                    MessageBox.Show("Nie wybrano żadnej drukarki.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                foreach (var selectedPrinter in listBox1.SelectedItems)
+                {
+                    var printer = printers.Find(p => p.printerName == selectedPrinter.ToString());
+                    if (printer != default)
+                    {
+                        selectedPrinters.Add(printer);
+                    }
+                }
+            }
+
+            foreach (var printer in selectedPrinters)
+            {
+                string result = AddPrinter(printer.printerName, printer.ipAddress);
+                log.AppendLine(result);
+            }
+
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string logPath = Path.Combine(documentsPath, "logi_drukarki.txt");
+            File.AppendAllText(logPath, $"{DateTime.Now:G}\n{log}\n");
+
+            button1.BackColor = originalColor;
+            button1.Enabled = true;
+            Cursor.Current = Cursors.Default;
+
+            MessageBox.Show(log.ToString(), "Wynik", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private string AddPrinter(string printerName, string printerIP)
+        {
+            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string driverPath = Path.Combine(appDirectory, "KOAX7J__.inf");
+
+            if (!File.Exists(driverPath))
+            {
+                return $"{printerName}: Nie znaleziono pliku sterownika w: {driverPath}";
+            }
+
+            var installDriverStartInfo = new ProcessStartInfo
+            {
+                FileName = @"C:\Windows\Sysnative\pnputil.exe",
+                Arguments = $"/add-driver \"{driverPath}\" /install",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using (var process = Process.Start(installDriverStartInfo))
+            {
+                string errorOutput = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrWhiteSpace(errorOutput))
+                {
+                    return $"{printerName}: Błąd podczas instalacji sterownika .inf: {errorOutput}";
+                }
+            }
+
+            string driverBaseName = "KONICA MINOLTA Universal";
+            var getDriverStartInfo = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -Command \"Get-PrinterDriver | Where-Object {{$_.Name -like '{driverBaseName}*'}} | Select-Object -First 1 -ExpandProperty Name\"",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            string resolvedDriverName;
+            using (var process = Process.Start(getDriverStartInfo))
+            {
+                resolvedDriverName = process.StandardOutput.ReadToEnd().Trim();
+                string errorOutput = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (string.IsNullOrWhiteSpace(resolvedDriverName))
+                {
+                    return $"{printerName}: Nie znaleziono pasującego sterownika drukarki (szukano: '{driverBaseName}*')";
+                }
+            }
+
+            var addPortStartInfo = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -Command \"if (-not (Get-PrinterPort -Name 'TCPIP_{printerName}' -ErrorAction SilentlyContinue)) " +
+                            $"{{ Add-PrinterPort -Name 'TCPIP_{printerName}' -PrinterHostAddress {printerIP} }}\"",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using (var process = Process.Start(addPortStartInfo))
+            {
+                string errorOutput = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrWhiteSpace(errorOutput))
+                {
+                    return $"{printerName}: Błąd podczas dodawania portu: {errorOutput}";
+                }
+            }
+
+            var addPrinterStartInfo = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -Command \"if (-not (Get-Printer -Name '{printerName}' -ErrorAction SilentlyContinue)) " +
+                    $"{{ Add-Printer -Name '{printerName}' -PortName 'TCPIP_{printerName}' -DriverName '{resolvedDriverName}' }}\"",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using (var process = Process.Start(addPrinterStartInfo))
+            {
+                string errorOutput = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrWhiteSpace(errorOutput))
+                {
+                    return $"{printerName}: Błąd podczas dodawania drukarki: {errorOutput}";
+                }
+            }
+
+            return $"{printerName}: Drukarka dodana pomyślnie!";
+        }
+
+        private bool IsRunningAsAdministrator()
+        {
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        private void pomocToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string instrukcja = "Instrukcja użytkowania:\n\n" +
+                                "1. Wybierz z listy drukarki, które chcesz dodać.\n" +
+                                "2. Kliknij przycisk 'Dodaj'.\n" +
+                                "3. Poczekaj, aż program zakończy instalację sterowników oraz konfigurację drukarki.\n" +
+                                "4. Po zakończeniu instalacji drukarka będzie gotowa do użycia.\n\n" +
+                                "W przypadku pytań lub problemów, prosimy o kontakt: wsparcie@ibe.edu.pl";
+            MessageBox.Show(instrukcja, "Pomoc", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string info = "Nazwa aplikacji: Instalator drukarek IBE\n" +
+                  "Wersja: 1.0.0\n" +
+                  "Autor: Jakub Wojciechowski\n" +
+                  "Instytucja: Instytut Badań Edukacyjnych\n\n" +
+                  "Aplikacja umożliwia szybkie i automatyczne dodawanie drukarek wraz z odpowiednimi sterownikami.\n\n" +
+                  "Wszelkie pytania i zgłoszenia błędów prosimy kierować na adres: wsparcie@ibe.edu.pl";
+            MessageBox.Show(info, "Informacje o aplikacji", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Zareaguj na zmianę wybranego elementu w ListBox
+            MessageBox.Show($"Wybrano: {listBox1.SelectedItem}");
+        }
+    }
+}
